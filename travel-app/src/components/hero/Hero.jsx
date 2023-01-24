572e9c508361f6af68df8bea5829c25e9e893f70
WanyonyiKenya <brianwanyonyike@gmail.com>
2023-01-24 17:14:11 +0300

    assets

---
+++ travel-app/src/components/hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img alt='hero-img'
+             src=''
+        />
+        <div className='text'>
+            <h1>Experience the world with Masafu Trips</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
